7ec2e2327d47c905617ac929166f2065a2b07d08
SergioHernandezRedondo <sergioorio04@gmail.com>
2026-01-29 17:17:12 +0100

    Pie chart added

--- main/source/app.py
+++ main/source/app.py
@@ -3,25 +3,43 @@ import dash
 from dash import dcc, html, Input, Output
 import plotly.express as px
 import dash_mantine_components as dmc
+from utils.utils import *
 
 df = pd.read_excel("../../data/CO2.xlsx", sheet_name="fossil_CO2_totals_by_country")
 columns = df.columns.to_list()
-years = []
+
+df_top5 = get_top5_countries()
 
 BACKGROUND_COLOR = "#101925"
+BORDER_COLOR = "#141F2E"
 
-for col in columns:
-    try:
-        years.append(int(col))
-    except ValueError:
-        pass
+min_year, max_year, years = get_years(df)
 
-if years:
-    min_year = min(years)
-    max_year = max(years)
-else:
-    print("ERROR: no hay años en la lista de columnas")
-    exit(1)
+fig_pie = px.pie(
+    df_top5,
+    names="Country",         # etiquetas del pastel
+    values="suma",   # valores que determinan tamaño de cada porción
+    hole=0.4                   # hace un donut en vez de un pastel completo (opcional)
+)
+
+fig_pie.update_traces(
+    marker=dict(line=dict(color='#233D38', width=2)),
+    textposition="inside",    # pone los valores dentro de cada sector
+    textinfo="percent+label", # muestra porcentaje + nombre
+    textfont=dict(
+        color='white',  # color de todos los textos
+        size=14,        # tamaño
+        family='Arial'  # fuente
+    )
+    # pull=[0.05, 0.05, 0.05, 0.05, 0.05, 0.05]   # sacar porciones
+)
+
+fig_pie.update_layout(
+    paper_bgcolor = BACKGROUND_COLOR,
+    plot_bgcolor="white",
+)
+
+fig_pie.update_layout(showlegend=False)
 
 app = dash.Dash(__name__)
 
@@ -31,7 +49,7 @@ app.layout = html.Div([
 
         # Título de la sección
         html.H2(
-            "Emisiones de CO₂ por país y año",
+            "CO2 Emissions by Country and Year",
             style={
                 "textAlign": "center",
                 "color": "white",
@@ -70,6 +88,35 @@ app.layout = html.Div([
         "borderRadius": "12px",
         "position": "relative"
     }),
+    html.Div([
+        html.Hr(style={
+            'borderWidth': '1px',      # grosor de la línea
+            'borderColor': BORDER_COLOR,    # color de la línea
+            'margin': '20px 0',        # espacio arriba y abajo
+            'width': '100%',            # ancho de la línea
+            'borderStyle': 'solid'     # estilo: solid, dashed, dotted
+        })
+    ], style = {
+        "marginTop": "100px",
+        "marginBottom": "100px"
+    }),
+    html.Div([
+        html.H2(
+            "Top 5 Countries with the Most Historical Emissions",
+            style={
+                "textAlign": "center",
+                "color": "white",
+                "marginBottom": "0px"
+            }
+        ),
+        dcc.Graph(
+            id="pie-chart",
+            figure=fig_pie,
+            style={"height": "60vh", "width": "60vw", "margin": "0 auto"}
+        )
+    ], style = {
+        "backgroundColor": BACKGROUND_COLOR
+    }),
 
 ],
 style={
@@ -103,7 +150,7 @@ def update_map(selected_year):
         showlakes=True,
         lakecolor=BACKGROUND_COLOR,
         showframe=True,
-        framecolor = "#e6f2ff" #DESCOMENTAR Y PONER EL DE ARRIBA TRUE SI SE QUIERE EL MAPA CON UN BORDE DELIMITADOR
+        framecolor = "#e6f2ff" #DESCOMENTAEmisiones de CO₂ por país y añoR Y PONER EL DE ARRIBA TRUE SI SE QUIERE EL MAPA CON UN BORDE DELIMITADOR
     )
     fig.update_layout(
         paper_bgcolor=BACKGROUND_COLOR
--- main/source/utils/utils.py
+++ main/source/utils/utils.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-def open_totals_country(path = "../../../data/CO2.xlsx", name = "fossil_CO2_totals_by_country"):
+def open_totals_country(path = "../../data/CO2.xlsx", name = "fossil_CO2_totals_by_country"):
     return pd.read_excel(path, sheet_name=name)
-def open_capita(path = "../../../data/CO2.xlsx", name ="fossil_CO2_per_capita_by_countr"):
+def open_capita(path = "../../data/CO2.xlsx", name ="fossil_CO2_per_capita_by_countr"):
     return pd.read_excel(path, sheet_name=name)
-def open_sector(path = "../../../data/CO2.xlsx", name = "fossil_CO2_by_sector_and_countr"):
+def open_sector(path = "../../data/CO2.xlsx", name = "fossil_CO2_by_sector_and_countr"):
     return pd.read_excel(path, sheet_name=name)
 
 def sum_co2_countries():
     """
     Suma toda la contaminación por cada país de los datos existentes
-    :return: matriz (n, 2): primer elemento nombre pais, segundo elemento suma
+    :return: DataFrame (n, 2): primer elemento nombre pais, segundo elemento suma
     """
     df = open_totals_country()
     names = df["Country"]
-
-    suma = df.loc[:, 'B':'F'].sum(axis=1)
+    min_year, max_year, _ = get_years(df)
+    suma = df.loc[:, min_year:max_year].sum(axis=1)
 
     return pd.concat([names, suma.rename("suma")], axis=1)
 
@@ -28,7 +28,10 @@ def sum_co2_all_countries():
     return df["suma"].sum()
 
 def get_top5_countries():
-
+    """
+    Selecciona los 5 paises con más contaminación histórica, también añade otra fila con los demás países
+    :return: DataFrame de los 5 paises con su contaminacion y los otros
+    """
     df = sum_co2_countries()
     total = sum_co2_all_countries()
 
@@ -38,6 +41,24 @@ def get_top5_countries():
 
     others = total - top5_sum
 
-    new_line = pd.DataFrame({"Country": ["Others"], "Suma": [others]})
+    new_line = pd.DataFrame({"Country": ["Others"], "suma": [others]})
 
     return pd.concat([top5, new_line], ignore_index = True)
+
+def get_years(df):
+    columns = df.columns.to_list()
+    years = []
+
+    for col in columns:
+        try:
+            years.append(int(col))
+        except ValueError:
+            pass
+
+    if years:
+        min_year = min(years)
+        max_year = max(years)
+    else:
+        print("ERROR: no hay años en la lista de columnas")
+        exit(1)
+    return min_year, max_year, years
